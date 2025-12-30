0e541c8841144ea6ac23e39615be0c2e418ca4ce
zaazew <fatimaahasanova@gmail.com>
2025-12-30 18:30:44 +0400

    logo seklini sixisdirdim cunki olcusu boyuk idi ve gec yuklenirdi. alerti silib bootstrap istifade etdim ve bu cssde bezi problemler yaratdi. ona gore yeniden bezi stil kodlarini tekrarlamali oldum. vibe ve shake hisselerinin jsni yazdim. bir de ickiler hemise eyni gelirdi onun ucun kod elave etdim ve artiq ferqli ickiler gelir her defe. bugunluk bu qeder dostlarr. bayyy

--- script.js
+++ script.js
@@ -3,29 +3,31 @@ let searchBtn=document.querySelector(".search-btn");
 let nameRadio=document.querySelector(".name-radio");
 let mainArea=document.querySelector(".main-area");
 let ingredientRadio=document.querySelector(".ingredient-radio");
+let burgerIcon=document.querySelector(".burger-icon");
+let mobileLinksContainer=document.querySelector(".mobile-links-container");
+let x=document.querySelector(".x");
+let moodBtns=document.querySelectorAll(".mood-list li");
+let shakeBtn=document.querySelector(".shake");
 
 
 /*-----------------------------------------------search----------------------------------------------------*/
 nameRadio.checked=true;
 searchInput.value=""
 searchBtn.addEventListener("click",()=>{
-    if(searchInput.value==""){
-        alert("Please enter something!")
+    if(searchInput.value.trim()==""){
+        var myModal = new bootstrap.Modal(document.getElementById('emptySearchModal'));
+        myModal.show();
         return;
     }
+
     let apiUrl=""
     if(nameRadio.checked){
-        apiUrl=`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput.value}`;
+        apiUrl=`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${encodeURIComponent(searchInput.value.trim())}`;
     }
     else{
-        apiUrl=`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchInput.value}`;
+        apiUrl=`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${encodeURIComponent(searchInput.value.trim())}`;
     }
-    fetch(apiUrl)
-    .then(response=>response.json())
-    .then(data=>cards(data))
-    .catch(error=>{                   
-        cards({drinks:null});
-    });
+    fetchUrl(apiUrl)
 })
 /* ingr radiosu secili ola ola inputa salmon yazdim ve main area bos qaldi.
 name ile axtaranda ve nese tapilmayanda hec ne tapilmadi cixir amma erzaq ile axtaranda bos qalir
@@ -43,7 +45,7 @@ function cards(data){
         </div>`
         return;
     }
-
+    data.drinks.sort(() => 0.5 - Math.random());// Sort funksiyasına random (+/-) dəyər verib siyahını təsadüfi qarışdırırıq (Shuffle)
     let sixDrinks=data.drinks.slice(0,6);
     sixDrinks.forEach(drink=>{
         let drinkCard=document.createElement("div")
@@ -65,8 +67,54 @@ function clearForRadio(){
 nameRadio.addEventListener("change",clearForRadio);
 ingredientRadio.addEventListener("change",clearForRadio);
 
+/*-----------------------------------------------mobile-links----------------------------------------------------*/
+burgerIcon.addEventListener("click",()=>{
+    mobileLinksContainer.style.display="block"
+})
+x.addEventListener("click",()=>{
+    mobileLinksContainer.style.display="none"
+})
 
+/*--------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------vibe----------------------------------------------------------*/
+/*https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list*/
+moodBtns.forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
+        searchInput.value=""
+        let text=e.target.innerText
+        switch(text){
+            case 'Party':
+            fetchUrl("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Punch / Party Drink")
+            break;
+            case 'Chill':
+            fetchUrl("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink")
+            break;
+            case 'Fancy':
+            fetchUrl("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+            break;
+            case 'No Alcohol':
+            fetchUrl("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
+            break;
+        }
+    })
 
+})
+/*--------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------fetch---------------------------------------------------------*/
+function fetchUrl(url){
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>cards(data))
+    .catch(error=>{                   
+        cards({drinks:null});
+    });
+}
+/*--------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------shake---------------------------------------------------------*/
+shakeBtn.addEventListener("click",()=>{
+    fetchUrl("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+})
+/*--------------------------------------------------------------------------------------------------------------*/
 
 
 
@@ -78,10 +126,8 @@ ingredientRadio.addEventListener("change",clearForRadio);
 
 /*Default olaraq sehifede gorunen icki hissesi*/
 function defaultCard(){
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=w")
-.then(response=>response.json())
-.then(data=>{
-    cards(data);
-})
+    fetchUrl("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=w")
 }
 defaultCard()
+
+
